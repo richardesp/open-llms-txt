4c24bb37b28b3a933663824a13c6ef2db68a0e62
Ricardo Espantaleón Pérez <respantaleonp@outlook.es>
2025-08-22 13:27:55 +0200

    docs: add reminder

--- src/open_llms_txt/generator/html_to_md.py
+++ src/open_llms_txt/generator/html_to_md.py
@@ -21,4 +21,4 @@ class HtmlToMdGenerator:
 
     def render(self, html: str, root_url: str = None, source_url: str = None) -> str:
         context = parse_html_to_json(html, root_url=root_url, source_url=source_url)
-        return self.template.render(engine="jinja2", **context)
+        return self.template.render(engine="jinja2", **context) # TODO: externalize the engine param with a predefined ENUM clause
